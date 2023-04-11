357832a5bb9afe12e48ed4bbcf5624784ffec0de
Daniel Dayley <ddayley@qualtrics.com>
2023-04-11 10:02:45 -0600

    Cleaned up chicken examples

--- persona/skills/chicken.py
+++ persona/skills/chicken.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 from typing import List
 import persona
 from persona import PersonaBaseSkill
-from dataclasses import dataclass
 import datetime
-import random
 import re
 
 class PersonaSkill(PersonaBaseSkill) :
-	"""A simple test skill that responds to the message 'Hello' with 'Hello!'"""
+	"""A simple test skill that responds to the joke 'Why did ____ cross the road?''"""
 	def match_intent(self,message: Message) -> Bool :
 		matches = re.search('^Why did the .* cross the road\?', message.text)
 		if matches :
